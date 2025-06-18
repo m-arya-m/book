5756fc510cd0b5f003429b036758455d14988c4c
maryam00 <maryamalkahlifa.sa@gmail.com>
2025-06-18 17:18:18 +0300

    add BookView

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
+from .models import Book
+from .serializers import BookSerializer
+
+from django.shortcuts import get_object_or_404
+from rest_framework import status
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
 # Create your views here.
+
+class BookCreatListView(APIView):
+
+    def get(self, request):
+        book = Book.objects.filter(user=request.user)
+        serializer = BookSerializer(book, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def post(self, request):
+        serializer = BookSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
